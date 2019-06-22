37389eebaec3fb744bd6256aca28ac3ad6ba6d95
lchambers6 <lukechambers91@gmail.com>
2019-06-21 22:49:19 -0400

    add registry page and fixed date countdown

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Sept 21, 2019 16:00:00").getTime();
+var countDownDate = new Date("Sept 21, 2019 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
